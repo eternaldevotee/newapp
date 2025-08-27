10409072d2dfa6fc605b6dadcf5049c13e863d84
Shubham <shubhamofficial0910@gmail.com>
2025-08-28 00:47:07 +0530

    Added signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+const helper:number = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[helper];
+  get img_path() {
+    return './users/' + this.selectedUser.avatar;
+
+  }
+
+  userClicked() {
+    const helper:number = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[helper];
+}
 
 }
